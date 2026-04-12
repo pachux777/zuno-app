14102c9ca1bec060f81ee7f4ad640e5d962b51f5
pachux777 <pachuff07@gmail.com>
2026-04-12 18:56:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,92 +4,75 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+const io = new Server(server);
 
 app.use(express.static("public"));
 
-/* ===== MATCH SYSTEM ===== */
+let waiting = [];
 
-let waitingQueue = [];
-
-function matchUsers(socket) {
-  if (waitingQueue.length > 0) {
-    const partner = waitingQueue.shift();
+function matchUser(socket){
+  if(waiting.length > 0){
+    const partner = waiting.shift();
 
     socket.partner = partner;
     partner.partner = socket;
 
     socket.emit("matched");
     partner.emit("matched");
-
-    console.log("Matched:", socket.id, partner.id);
   } else {
-    waitingQueue.push(socket);
-    console.log("Waiting:", socket.id);
+    waiting.push(socket);
   }
 }
 
-/* ===== SOCKET ===== */
-
-io.on("connection", (socket) => {
+io.on("connection",(socket)=>{
 
   console.log("User connected:", socket.id);
 
-  socket.on("start", () => {
-    matchUsers(socket);
+  socket.on("start",()=>{
+    matchUser(socket);
   });
 
-  socket.on("message", (msg) => {
-    if (socket.partner) {
-      socket.partner.emit("message", {
-        name: "Stranger",
-        text: msg
+  socket.on("message",(msg)=>{
+    if(socket.partner){
+      socket.partner.emit("message",{
+        name:"Stranger",
+        text:msg
       });
     }
   });
 
-  socket.on("typing", () => {
-    if (socket.partner) {
-      socket.partner.emit("typing");
-    }
-  });
-
-  socket.on("next", () => {
-    if (socket.partner) {
+  socket.on("next",()=>{
+    if(socket.partner){
       socket.partner.partner = null;
       socket.partner.emit("partner-disconnected");
     }
     socket.partner = null;
-    matchUsers(socket);
+    matchUser(socket);
   });
 
-  socket.on("end", () => {
-    if (socket.partner) {
+  socket.on("end",()=>{
+    if(socket.partner){
       socket.partner.emit("partner-disconnected");
       socket.partner.partner = null;
     }
     socket.partner = null;
   });
 
-  socket.on("disconnect", () => {
-    waitingQueue = waitingQueue.filter(u => u !== socket);
+  socket.on("disconnect",()=>{
+    waiting = waiting.filter(u => u !== socket);
 
-    if (socket.partner) {
+    if(socket.partner){
       socket.partner.emit("partner-disconnected");
       socket.partner.partner = null;
     }
 
-    console.log("Disconnected:", socket.id);
+    console.log("User disconnected:", socket.id);
   });
 
 });
 
-/* ===== SERVER START ===== */
-
 const PORT = process.env.PORT || 10000;
 
-server.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT,"0.0.0.0",()=>{
   console.log("Server running on port", PORT);
 });
